82aef3534e1fe26f1a864dbc4e285258cb84c405
Muhhamad Affan Hasby <38319387+affanhasby@users.noreply.github.com>
2020-10-14 00:08:25 +0700

    cobacoba

--- js/Extension.js
+++ js/Extension.js
@@ -23,15 +23,16 @@ class Extension {
 		let buttonOperator = null
 		let forwarded_number = null
 		var table = $('#tb').DataTable();
-		let btnDelete = "<img id=\"id-delete-extension-" + extension["id_extension"] + "\" alt=\"Icon for deleting\" src=\"res/ic_trash.png\" style=\"width: 20px; margin-left: 5px;\">" ;
-		let btnUpdate = "<img id=\"id-click-update-extension-" + extension["id_extension"] + "\" alt=\"Icon for updating\" src=\"res/ic_edit.png\" style=\"width: 20px; margin-left: 5px;\">";
-		let btnTopUp = "<img id=\"id-click-top-up-extension-" + extension["id_extension"] + "\" alt=\"Icon for top up\" src=\"res/ic_top_up.png\" style=\"width: 20px; margin-left: 5px;\">";
+		
 		for (let extension of data) {
 			if (extension["operator"] === 1) {
 				operatorEnabled = true
 			}
 		}
 		for (let extension of data) {
+			let btnDelete = "<img id=\"id-delete-extension-" + extension["id_extension"] + "\" alt=\"Icon for deleting\" src=\"res/ic_trash.png\" style=\"width: 20px; margin-left: 5px;\">" ;
+			let btnUpdate = "<img id=\"id-click-update-extension-" + extension["id_extension"] + "\" alt=\"Icon for updating\" src=\"res/ic_edit.png\" style=\"width: 20px; margin-left: 5px;\">";
+			let btnTopUp = "<img id=\"id-click-top-up-extension-" + extension["id_extension"] + "\" alt=\"Icon for top up\" src=\"res/ic_top_up.png\" style=\"width: 20px; margin-left: 5px;\">";
 			number = number + 1;
 			if (extension["operator"] === 1 && operatorEnabled) {
 				buttonOperator = 
