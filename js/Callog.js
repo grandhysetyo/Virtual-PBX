404fa8717714c3f128f3b811e351717a21932f95
Grandhys Setyo Utomo <setyo.grandhys1@gmail.com>
2020-10-14 12:25:35 +0700

    Update Callog.js

--- js/Callog.js
+++ js/Callog.js
@@ -30,7 +30,9 @@ $(document).ready(function () {
     GLOBAL.connection.getAllPbxs(function(data) {
         callog.displayAllPbxs(data);
     });
-
+    GLOBAL.connection.getCallLogData(null, null, 98, null, function (data) {
+        callog.displayCallLogData(data);
+    });
     var activities = document.getElementById("pbx-name");
     activities.addEventListener("change", function() {
         if(activities.value != "")
