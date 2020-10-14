316d14a35b0516ce3eea3aa075dab3548b512635
Grandhys Setyo Utomo <setyo.grandhys1@gmail.com>
2020-10-14 10:49:48 +0700

    update page callog

--- js/Callog.js
+++ js/Callog.js
@@ -16,12 +16,33 @@ class Callog {
 		}
 		
     }
+
+    displayCallLogData(data) {
+		var table = $('#tb').DataTable();
+		for (let callLog of data) {			
+            table.row.add([callLog[0],callLog[1],callLog[2],callLog[3],callLog[4],callLog[5]]).draw();    
+		}
+		$('#callLogs').DataTable().draw();;
+
+	}
     
 }
 $(document).ready(function () {
     let callog = new Callog();     
     GLOBAL.connection.getAllPbxs(function(data) {
         callog.displayAllPbxs(data);
+    });
+
+    var activities = document.getElementById("pbx-name");
+    activities.addEventListener("change", function() {
+        if(activities.value != "")
+        {            
+            var idpbx = activities.options[activities.selectedIndex].value;            
+            GLOBAL.connection.getCallLogData(null, null, idpbx, null, function (data) {
+                EXTENSION.displayCallLogData(data);
+            });
+        }
+        
     });
 });
 
